54a751be43ebdf982b10fa55c00040bf48463b25
Soham Kumar Dey <93932583+SohamDey80@users.noreply.github.com>
2025-11-30 16:42:28 +0530

    Add files via upload

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Playfair Display", "serif"],
+        sans: ["Inter", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+};
